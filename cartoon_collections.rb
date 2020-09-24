7540799f51bcc0f631eb7f70fc6815c096b860fa
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-24 15:32:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,16 +9,16 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  
   planeteer_calls.map do |element|
      element.capitalize + "!"
   end
-  
 end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  planeteer_calls.each do |element|
+    element
 end
 
 def find_valid_calls(planeteer_calls)
